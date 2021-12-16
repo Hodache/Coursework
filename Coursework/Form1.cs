f187a54d15cd868a62190ae3bb03801f60b892ae
Денис Афанасьев <hodache@yandex.ru>
2021-12-16 16:39:51 +0800

    Добавьте файлы проекта.

---
+++ Coursework/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coursework
+{
+    public partial class Form1 : Form
+    {
+        static List<Particle> particles = new List<Particle>(); // Список частиц
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Привязка изображения к pb
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // Генерация частиц
+            for (var i = 0; i < 500; i++) {
+                var particle = new Particle();
+
+                particle.X = picDisplay.Width / 2;
+                particle.Y = picDisplay.Height / 2;
+
+                particles.Add(particle);
+            }
+        }
+
+        // Пересчет состояния системы
+        private void UpdateState()
+        {
+
+        }
+
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White); 
+                
+                foreach(var particle in particles)
+                {
+                    particle.Draw(g);
+                }
+            }
+
+            // Обновление pb
+            picDisplay.Invalidate();
+        }
+    }
+}
---
+++ Coursework/Particle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Coursework
+{
+    class Particle
+    {
+        public int Radius; // Радиус частицы
+        public float X; // Координата x
+        public float Y; // Координата y
+        public float Direction; // Направление движения в градусах
+        public float Speed; // Скорость перемещения
+
+        public static Random rnd = new Random();
+
+        public Particle()
+        {
+            Direction = rnd.Next(360);
+            Speed = 1 + rnd.Next(10);
+            Radius = 2 + rnd.Next(10);
+        }
+
+        public void Draw(Graphics g)
+        {
+            var brush = new SolidBrush(Color.Black); // Кисть
+
+            // Отрисовка частицы
+            g.FillEllipse(brush, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            brush.Dispose();
+        }
+    }
+}
